25a1cadfb89c37231f88c4b541cc4aef0006cdb7
Yoosaf <myoosaf34@email.com>
2026-04-20 15:19:18 +0530

    R2-url-fix

--- src/services/roomType.service.ts
+++ src/services/roomType.service.ts
@@ -89,7 +89,7 @@ export async function deleteRoomType({
 
 const s3 = new S3Client({
   region: "auto",
-  endpoint: process.env.R2_ENDPOINT!,
+  endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
     accessKeyId:     process.env.R2_ACCESS_KEY_ID!,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
