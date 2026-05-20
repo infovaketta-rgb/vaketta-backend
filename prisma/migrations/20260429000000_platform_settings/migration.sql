9d1b979818808cacc20e2d0f814904add6eadc1b
Yoosaf <myoosaf34@email.com>
2026-05-20 22:39:11 +0530

    DB-change

--- prisma/migrations/20260429000000_platform_settings/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "PlatformSettings" (
-    "id" TEXT NOT NULL DEFAULT 'global',
-    "instagramEmbedUrl" TEXT NOT NULL DEFAULT '',
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "PlatformSettings_pkey" PRIMARY KEY ("id")
-);
